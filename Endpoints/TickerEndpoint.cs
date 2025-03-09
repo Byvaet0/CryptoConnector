c14d8441bebcbc9655e58e841320b8814f70a2bc
Anton Pyatnichko <anris1901@gmail.com>
2025-03-09 22:07:43 +0300

    Commit1

---
+++ Endpoints/TickerEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CryptoConnector.BitfinexClient
+{
+    public class TickerEndpoint
+    {
+
+        public async Task<string> GetTickerAsync(string pair)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://api-pub.bitfinex.com/v2/ticker/t{pair}"),
+                Headers =
+    {
+        { "accept", "application/json" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                return body;
+            }
+        }
+
+    }
+}
---
+++ Endpoints/TradesEndpoint.cs
@@ -0,0 +1,51 @@
+﻿using CryptoConnector.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CryptoConnector.Endpoints
+{
+    public class TradesEndpoint
+    {
+
+        public async Task<List<Trade>> GetTradesAsync(string pair, int maxCount)
+        {
+            var client = new HttpClient();
+            var trades = new List<Trade>();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://api-pub.bitfinex.com/v2/trades/t{pair}/hist?limit={maxCount}&sort=-1"),
+                Headers =
+                {
+                    { "accept", "application/json" },
+                },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var jsonTrades = JsonSerializer.Deserialize<List<List<double>>>(body);
+                
+                foreach (var trade in jsonTrades)
+                {
+                    
+                    trades.Add(new Trade
+                    {
+                        Pair = pair,
+                        Price = (decimal)trade[3],
+                        Amount = (decimal)trade[2],
+                        Side =  ((decimal)trade[2] > 0) ? "buy" : "sell",
+                        Time = DateTimeOffset.FromUnixTimeMilliseconds((long)trade[1]),
+                        Id = trade[0].ToString()
+                    });
+                }
+                return trades;
+
+            }
+        }
+    }
+}
